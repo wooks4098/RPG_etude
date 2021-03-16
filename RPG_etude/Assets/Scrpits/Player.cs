bbce41d4a13fe094f1dccdbd521611166ce7bb45
wooks4098 <70507138+wooks4098@users.noreply.github.com>
2021-03-16 18:01:33 +0900

    Player Move

--- RPG_etude/Assets/Scrpits/Player.cs
+++ RPG_etude/Assets/Scrpits/Player.cs
@@ -19,51 +19,93 @@ public class Player : MonoBehaviour
     private void Awake()
     {
         camera = Camera.main;
-        animator = GetComponent<Animator>();
+        animator = GetComponentInChildren<Animator>();
         agent = GetComponent<NavMeshAgent>();
+
+        //NavMeshAgent회전 제한
         agent.updateRotation = false;
     }
 
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        if(Input.GetMouseButton(1))
+       
+
+        Move();
+        EndMoveCheck();
+    }
+
+    void Move()
+    {
+        if (Input.GetMouseButtonDown(1))
+            agent.velocity = Vector3.zero;
+        if (Input.GetMouseButton(1))
         {
             RaycastHit hit;
-            if(Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition),out hit))
+            if (Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition), out hit))
             {
-                setDestination(hit.point);
+                if (Vector3.Distance(animator.transform.position, hit.point) > 0.2f)
+                {
+
+                   
+                    //이동
+                    // agent.velocity = Vector3.zero;
+                    agent.SetDestination(hit.point);
+                    destination = hit.point;
+                    isMove = true;
+                    animator.SetBool("isMove", true);
+                }
+
             }
         }
-        LookMoveDirateion();
     }
+           
 
-    void setDestination(Vector3 dest)
+    void EndMoveCheck()
     {
-        agent.SetDestination(dest);
-
-        destination = dest;
-        isMove = true;
-        animator.SetBool("isMove", true);
-    }
-
-    void LookMoveDirateion()
-    {
-        if(isMove) 
+        if (isMove)
         {
-            if (agent.velocity.magnitude == 0f)
+            if(agent.velocity.sqrMagnitude >= 0.1f * 0.1f && agent.remainingDistance <= 0.1f)
             {
                 isMove = false;
                 animator.SetBool("isMove", false);
-                return;
             }
-            var dir = new Vector3(agent.steeringTarget.x, transform.position.y, agent.steeringTarget.z) - transform.position;
-            transform.forward = dir;
-            //transform.position += dir.normalized * Time.deltaTime * Speed;
+            else if (agent.desiredVelocity.sqrMagnitude >= 0.1f * 0.1f)
+            {
+                //에이전트의 이동방향
+                Vector3 direction = agent.desiredVelocity;
+                //회전각도(쿼터니언)산출
+                Quaternion targetangle = Quaternion.LookRotation(direction);
+                //선형보간 함수를 이용해 부드러운 회전
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetangle, Time.deltaTime * 8.0f);
+            }
+           
+
+
+
+
+
+            //if (agent.velocity.magnitude <= 0.2f)
+            //{
+            //    isMove = false;
+            //    animator.SetBool("isMove", false);
+            //    return;
+            //}
+
+            ////Vector3 dir = agent.desiredVelocity;
+            ////Quaternion targetangle = Quaternion.LookRotation(dir);
+            ////animator.transform.rotation = Quaternion.Slerp(transform.rotation, targetangle, Time.deltaTime * 8.0f);
+
+
+
+
+            //var dir = new Vector3(agent.steeringTarget.x, transform.position.y, agent.steeringTarget.z) - transform.position;
+            //animator.transform.forward = dir;
+
         }
 
     }
