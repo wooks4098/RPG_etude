3d33daa34fc662d7fc94cdbbdc6448335f124444
wooks4098 <70507138+wooks4098@users.noreply.github.com>
2021-05-14 05:41:01 +0900

    플레이어 이벤트

---
+++ RPG_etude/Assets/Scrpits/Player/Player_Event_Manager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Event_Manager : MonoBehaviour
+{
+    private static Player_Event_Manager instance = null;
+
+    void Awake()
+    {
+        if (null == instance)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+            Destroy(this.gameObject);
+    }
+
+    public static Player_Event_Manager Instance
+    {
+        get
+        {
+            if (null == instance)
+            {
+                return null;
+            }
+            return instance;
+        }
+    }
+
+    #region 이벤트
+
+    #region 몬스터 처치
+    public void Kill_Slime()
+    {
+
+    }
+    #endregion
+
+
+    #region 아이템 획득
+
+    #endregion
+
+
+
+    #endregion
+}
